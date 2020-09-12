58b8d3857f3106e2a9782e835cab9e027c3bccd8
kiwi <schiff.trevor@gmail.com>
2020-09-12 00:49:04 -0400

    Rename label in .ctors
    lbl_80355080 -> global_ctors

--- asm/ctors.s
+++ asm/ctors.s
@@ -2,8 +2,8 @@
 
 .section .ctors, "a"  # 0x80355080 - 0x80355260
 
-.global lbl_80355080
-lbl_80355080:
+.global global_ctors
+global_ctors:
 .4byte func_800B2288
 .4byte __sinit_$$3ut_IOStream_cpp
 .4byte __sinit_$$3ut_FileStream_cpp
