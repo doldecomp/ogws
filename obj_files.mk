58c5a65f1c39b8ea8ab02defb6bb7cb5ab08fdd7
kiwi <schiff.trevor@gmail.com>
2020-09-13 01:16:06 -0400

    Labeled snd_AxVoice

--- obj_files.mk
+++ obj_files.mk
@@ -64,6 +64,7 @@ TEXT_O_FILES := \
 	build/asm/nw4r/math/math_types.o \
 	build/asm/nw4r/math/math_geometry.o \
 	build/asm/nw4r/snd/snd_AxManager.o \
+	build/asm/nw4r/snd/snd_AxVoice.o \
 	build/asm/text.o \
 
 CTORS_O_FILES := build/asm/ctors.o
