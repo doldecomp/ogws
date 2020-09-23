6d448ad306fa2b62b9f328b958016d4b2cd059cc
kiwi <schiff.trevor@gmail.com>
2020-09-23 18:17:49 -0400

    snd_BasicSound labeling, ctors_1 + dtors labeling

--- asm/dtors.s
+++ asm/dtors.s
@@ -4,6 +4,6 @@
 
 .global global_dtors
 global_dtors:
-.4byte func_800B184C
-.4byte func_800B22C4
+.4byte func_800B184C # __destroy_global_chain
+.4byte func_800B22C4 # __fini_cpp_exceptions
 .fill 0x18
