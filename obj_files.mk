e58d5ed88fb3269e9fee74c9656dd12e284dc732
kiwi <schiff.trevor@gmail.com>
2020-11-28 04:09:03 -0500

    Labeled snd_WsdFile.s

--- obj_files.mk
+++ obj_files.mk
@@ -124,6 +124,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/snd/snd_WaveFile.o \
 	build/asm/nw4r/snd/snd_WaveSound.o \
 	build/asm/nw4r/snd/snd_WaveSoundHandle.o \
+	build/asm/nw4r/snd/snd_WsdFile.o \
 
 
 GROUP_1_O_FILES := \
