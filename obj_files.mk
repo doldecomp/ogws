24ebed2f3873ffb169370e58ca200a332e4d3868
kiwi <schiff.trevor@gmail.com>
2021-01-18 22:43:49 -0500

    Labeled g3d_anmtexpat.s

--- obj_files.mk
+++ obj_files.mk
@@ -145,7 +145,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_reslightset.o \
 	build/asm/nw4r/g3d/g3d_anmvis.o \
 	build/asm/nw4r/g3d/g3d_anmclr.o \
-
+	build/asm/nw4r/g3d/g3d_anmtexpat.o \
 
 GROUP_1_O_FILES := \
 	build/asm/text.o \
