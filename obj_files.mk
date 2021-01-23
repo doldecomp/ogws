3a4d31a071342d9671d9a5d581b0ca1d0385b675
kiwi <schiff.trevor@gmail.com>
2021-01-23 18:08:23 -0500

    Labeled g3d_dcc.s

--- obj_files.mk
+++ obj_files.mk
@@ -158,6 +158,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_state.o \
 	build/asm/nw4r/g3d/g3d_draw1mat1shp.o \
 	build/asm/nw4r/g3d/g3d_calcview.o \
+	build/asm/nw4r/g3d/g3d_dcc.o \
 
 
 GROUP_1_O_FILES := \
