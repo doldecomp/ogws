cb91618693d57c333674809c197564255c89407f
kiwi <schiff.trevor@gmail.com>
2021-01-23 17:45:50 -0500

    Labeled g3d_state.s

--- obj_files.mk
+++ obj_files.mk
@@ -155,6 +155,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_anmobj.o \
 	build/asm/nw4r/g3d/g3d_gpu.o \
 	build/asm/nw4r/g3d/g3d_cpu.o \
+	build/asm/nw4r/g3d/g3d_state.o \
 
 
 GROUP_1_O_FILES := \
