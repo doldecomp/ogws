657bf3164b29592fc0aaf7bb6cecf73ce7b231db
kiwi <schiff.trevor@gmail.com>
2021-01-24 16:50:20 -0500

    Labeled g3d_camera.s

--- obj_files.mk
+++ obj_files.mk
@@ -162,6 +162,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_workmem.o \
 	build/asm/nw4r/g3d/g3d_calcworld.o \
 	build/asm/nw4r/g3d/g3d_draw.o \
+	build/asm/nw4r/g3d/g3d_camera.o \
 
 
 GROUP_1_O_FILES := \
