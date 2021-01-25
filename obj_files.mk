10a6b46d6dbc5141b61b28dcc8bd6d705012d6ee
kiwi <schiff.trevor@gmail.com>
2021-01-25 01:38:58 -0500

    Labeled g3d_calcvtx.s

--- obj_files.mk
+++ obj_files.mk
@@ -176,6 +176,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_scnproc.o \
 	build/asm/nw4r/g3d/g3d_fog.o \
 	build/asm/nw4r/g3d/g3d_light.o \
+	build/asm/nw4r/g3d/g3d_calcvtx.o \
 
 
 GROUP_1_O_FILES := \
