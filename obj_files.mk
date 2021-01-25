4bef4b57c158de880ee54e8d91ea7e5c2e41b40b
kiwi <schiff.trevor@gmail.com>
2021-01-25 01:18:20 -0500

    Partially labeled g3d_scnproc.s

--- obj_files.mk
+++ obj_files.mk
@@ -173,6 +173,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_scnmdl.o \
 	build/asm/nw4r/g3d/g3d_calcmaterial.o \
 	build/asm/nw4r/g3d/g3d_init.o \
+	build/asm/nw4r/g3d/g3d_scnproc.o \
 
 
 GROUP_1_O_FILES := \
