028541cdb53db057b06ca516c5e58380f2b94cea
kiwi <schiff.trevor@gmail.com>
2021-01-25 01:34:23 -0500

    Labeled g3d_light.s

--- obj_files.mk
+++ obj_files.mk
@@ -175,6 +175,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_init.o \
 	build/asm/nw4r/g3d/g3d_scnproc.o \
 	build/asm/nw4r/g3d/g3d_fog.o \
+	build/asm/nw4r/g3d/g3d_light.o \
 
 
 GROUP_1_O_FILES := \
