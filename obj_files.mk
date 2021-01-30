fa355a1ffdd2ae484e46e89b116389f1cc87d5a4
kiwi <schiff.trevor@gmail.com>
2021-01-30 15:32:27 -0500

    Labeled lyt_textBox.s

--- obj_files.mk
+++ obj_files.mk
@@ -181,6 +181,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/lyt/lyt_group.o \
 	build/asm/nw4r/lyt/lyt_layout.o \
 	build/asm/nw4r/lyt/lyt_picture.o \
+	build/asm/nw4r/lyt/lyt_textBox.o \
 
 GROUP_1_O_FILES := \
 	build/asm/text.o \
