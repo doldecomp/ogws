559640f40ff4b3154df903b663e1ef023a7a41a9
kiwi <schiff.trevor@gmail.com>
2021-01-30 16:18:04 -0500

    Labeled lyt_arcResourceAccessor.s

--- obj_files.mk
+++ obj_files.mk
@@ -189,6 +189,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/lyt/lyt_drawInfo.o \
 	build/asm/nw4r/lyt/lyt_animation.o \
 	build/asm/nw4r/lyt/lyt_resourceAccessor.o \
+	build/asm/nw4r/lyt/lyt_arcResourceAccessor.o \
 
 GROUP_1_O_FILES := \
 	build/asm/text.o \
