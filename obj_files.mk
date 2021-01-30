dd56735122e2bca5deee688fe44ec455f22461d5
kiwi <schiff.trevor@gmail.com>
2021-01-30 15:53:43 -0500

    Labeled lyt_drawInfo.s

--- obj_files.mk
+++ obj_files.mk
@@ -186,6 +186,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/lyt/lyt_bounding.o \
 	build/asm/nw4r/lyt/lyt_material.o \
 	build/asm/nw4r/lyt/lyt_texMap.o \
+	build/asm/nw4r/lyt/lyt_drawInfo.o \
 
 GROUP_1_O_FILES := \
 	build/asm/text.o \
