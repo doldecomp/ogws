79b335f6c83782ab389cb947768d21f604d8a23c
kiwi <schiff.trevor@gmail.com>
2021-01-30 15:16:22 -0500

    Labeled lyt_group.s

--- obj_files.mk
+++ obj_files.mk
@@ -178,7 +178,7 @@ NW4R_O_FILES := \
 	build/asm/nw4r/g3d/g3d_light.o \
 	build/asm/nw4r/g3d/g3d_calcvtx.o \
 	build/asm/nw4r/lyt/lyt_pane.o \
-
+	build/asm/nw4r/lyt/lyt_group.o \
 
 GROUP_1_O_FILES := \
 	build/asm/text.o \
