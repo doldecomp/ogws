71761a5507def3c4ddb54e681f4240e212adf2d6
kiwi <schiff.trevor@gmail.com>
2021-05-03 02:40:27 -0400

    Labeled `eggStream.s`

--- obj_files.mk
+++ obj_files.mk
@@ -206,6 +206,7 @@ EGG_O_FILES := \
 	build/asm/egg/core/eggColorFader.o \
 	build/asm/egg/core/eggSceneManager.o \
 	build/asm/egg/core/eggController.o \
+	build/asm/egg/core/eggStream.o \
 	build/asm/egg/egg_b.o \
 	build/asm/egg/audio/eggAudioSystem.o \
 	build/asm/egg/util/eggMsgRes.o \
