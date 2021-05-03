67fbf1d05b1a5dc039bd516de568b4987e678788
kiwi <schiff.trevor@gmail.com>
2021-05-03 17:38:13 -0400

    Labeled `eggAudioHeapMgr.s`

--- obj_files.mk
+++ obj_files.mk
@@ -227,6 +227,7 @@ EGG_O_FILES := \
 	build/asm/egg/audio/eggAudioArcPlayerMgr.o \
 	build/asm/egg/audio/eggAudioExpMgr.o \
 	build/asm/egg/audio/eggAudioFxMgr.o \
+	build/asm/egg/audio/eggAudioHeapMgr.o \
 	build/asm/egg/egg_b.o \
 	build/asm/egg/audio/eggAudioSystem.o \
 	build/asm/egg/util/eggMsgRes.o \
