97778d0b6a498719f6ae5ee2fb34290d3a2d72b9
kiwi <schiff.trevor@gmail.com>
2021-05-03 14:58:50 -0400

    Labeled `eggDisposer.s`

--- obj_files.mk
+++ obj_files.mk
@@ -219,6 +219,7 @@ EGG_O_FILES := \
 	build/asm/egg/core/eggDvdFile.o \
 	build/asm/egg/core/eggScene.o \
 	build/asm/egg/core/eggProcessMeter.o \
+	build/asm/egg/core/eggDisposer.o \
 	build/asm/egg/egg_b.o \
 	build/asm/egg/audio/eggAudioSystem.o \
 	build/asm/egg/util/eggMsgRes.o \
