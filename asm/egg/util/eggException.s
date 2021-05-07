51cfd2e7656ee91b3a85a3bd03035e00a4c72609
kiwi <schiff.trevor@gmail.com>
2021-05-07 11:38:04 -0400

    Update eggException.s

--- asm/egg/util/eggException.s
+++ asm/egg/util/eggException.s
@@ -2,7 +2,7 @@
 
 .section .text, "ax"
 
-# "Verified" symbolfrom BBA, call matched by WFU (RPSysSystem::initialize)
+# "Verified" symbol from BBA, call matched by WFU (RPSysSystem::initialize)
 # OGWS: Exception::create(0x40,0x20,4,0);
 # WFU:  Exception::create(0x40,0x20,4,0);
 .global create__Q23EGG9ExceptionFUsUsUsPQ23EGG4Heap
@@ -10,8 +10,8 @@ create__Q23EGG9ExceptionFUsUsUsPQ23EGG4Heap:
 /* 800B1744 000AC644  38 60 00 00 */	li r3, 0
 /* 800B1748 000AC648  4E 80 00 20 */	blr 
 
-# FUN_800b174c(&PTR_DAT_803820b8);
 # Given double pointer to floating_point_unavailable_exception_handler
-.global func_800B174C
-func_800B174C:
+# Non-stubbed version sets user exception callback
+.global setPadInfo__Q23EGG9ExceptionFPCUs
+setPadInfo__Q23EGG9ExceptionFPCUs:
 /* 800B174C 000AC64C  4E 80 00 20 */	blr 
