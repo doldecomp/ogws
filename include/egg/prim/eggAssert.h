efa24aef3a5c4c19dc6fdfa990f6ea2d322d1cce
kiwi <schiff.trevor@gmail.com>
2021-05-11 13:19:33 -0400

    Updated `EGG_ASSERT_MSG` to allow for a variadic `MSG`

--- include/egg/prim/eggAssert.h
+++ include/egg/prim/eggAssert.h
@@ -2,7 +2,7 @@
 #define EGG_PRIM_ASSERT_H
 
 #define EGG_ASSERT(EXP) (void)((EXP) || (system_halt(__FILE__, __LINE__, #EXP),0))
-#define EGG_ASSERT_MSG(MSG, EXP) (void)((EXP) || (system_halt(__FILE__, __LINE__, MSG),0))
+#define EGG_ASSERT_MSG(EXP, ...) (void)((EXP) || (system_halt(__FILE__, __LINE__, __VA_ARGS__),0))
 
 #ifdef __cplusplus
 extern "C" {
