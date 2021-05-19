b8f855c660cbc95a51a36d9cf19671b0501193ea
kiwi <schiff.trevor@gmail.com>
2021-05-19 18:21:50 -0400

    Update eggExpHeap.h

--- include/egg/core/eggExpHeap.h
+++ include/egg/core/eggExpHeap.h
@@ -14,9 +14,9 @@ namespace EGG
         virtual void * alloc(u32, s32); // at 0x14
         virtual void free(void *); // at 0x18
         virtual void destroy(); // at 0x1C
-        virtual UNKWORD resizeForMBlock(void *, u32); // at 0x20
-        virtual UNKWORD getAllocatableSize(s32); // at 0x24
-        virtual UNKWORD adjust(); // at 0x28
+        virtual u32 resizeForMBlock(void *, u32); // at 0x20
+        virtual u32 getAllocatableSize(s32); // at 0x24
+        virtual u32 adjust(); // at 0x28
 
         static ExpHeap * create(void *, u32, u16);
         static ExpHeap * create(u32, Heap *, u16);
