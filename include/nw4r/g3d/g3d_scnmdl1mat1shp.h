3b3f6a03bda3a799e725cad8a0200e20af2e86d7
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2021-07-26 19:51:39 -0400

    "Decompiled" `g3d_scnmdl1mat1shp.s`

---
+++ include/nw4r/g3d/g3d_scnmdl1mat1shp.h
@@ -0,0 +1,16 @@
+#ifndef NW4R_G3D_SCN_MDL1_MAT1_SHP_H
+#define NW4R_G3D_SCN_MDL1_MAT1_SHP_H
+#include "g3d_obj.h"
+
+namespace nw4r
+{
+    namespace g3d
+    {
+        class ScnMdl1Mat1Shp
+        {
+            NW4R_G3D_TYPE_OBJ_DECL(ScnMdl1Mat1Shp);
+        };
+    }
+}
+
+#endif
---
+++ src/nw4r/g3d/g3d_scnmdl1mat1shp.cpp
@@ -0,0 +1,9 @@
+#include "g3d_scnmdl1mat1shp.h"
+
+namespace nw4r
+{
+    namespace g3d
+    {
+        NW4R_G3D_TYPE_OBJ_DEF(ScnMdl1Mat1Shp);
+    }
+}
