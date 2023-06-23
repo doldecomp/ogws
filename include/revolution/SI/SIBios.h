17acf532daa9f2245064aada73b3a3d282287811
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-06-23 09:56:59 -0400

    Import SI headers from open_rvl

---
+++ include/revolution/SI/SIBios.h
@@ -0,0 +1,13 @@
+#ifndef RVL_SDK_SI_SIBIOS_H
+#define RVL_SDK_SI_SIBIOS_H
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void SIInit(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
