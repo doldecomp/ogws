07c8e1036eaeff314f1585101f8c6abe2a289163
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-06-23 09:55:38 -0400

    Import PAD headers from open_rvl

---
+++ include/revolution/PAD.h
@@ -0,0 +1,12 @@
+#ifndef RVL_SDK_PUBLIC_PAD_H
+#define RVL_SDK_PUBLIC_PAD_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <revolution/PAD/Pad.h>
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ include/revolution/PAD/Pad.h
@@ -0,0 +1,15 @@
+#ifndef RVL_SDK_PAD_H
+#define RVL_SDK_PAD_H
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern u32 __PADSpec;
+
+BOOL __PADDisableRecalibration(BOOL);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
