24505a370b27af27c54b2a33b500b8c052112d81
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-06-23 09:55:08 -0400

    Import NWC24 headers from open_rvl

---
+++ include/revolution/NWC24/NWC24Schedule.h
@@ -0,0 +1,13 @@
+#ifndef RVL_SDK_NWC24_SCHEDULE_H
+#define RVL_SDK_NWC24_SCHEDULE_H
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+s32 NWC24SuspendScheduler(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
