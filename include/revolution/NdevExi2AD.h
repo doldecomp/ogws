790050d780a4c64dc3516d9922dcb95de3534292
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-06-23 09:54:17 -0400

    Import NdevExi2AD headers from open_rvl

---
+++ include/revolution/NdevExi2AD.h
@@ -0,0 +1,14 @@
+#ifndef RVL_SDK_PUBLIC_NDEV_EXI2_AD_H
+#define RVL_SDK_PUBLIC_NDEV_EXI2_AD_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <revolution/NdevExi2AD/DebuggerDriver.h>
+#include <revolution/NdevExi2AD/ODEMU.h>
+#include <revolution/NdevExi2AD/exi2.h>
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ include/revolution/NdevExi2AD/ODEMU.h
@@ -0,0 +1,31 @@
+#ifndef RVL_SDK_NDEV_EXI2_AD_ODEMU_H
+#define RVL_SDK_NDEV_EXI2_AD_ODEMU_H
+#include <types.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline u32 ODEMUGenMailData(u32 ofs, u32 size) {
+    return (ofs & 0xff) << 0x10 | 0x1f000000 | size & 0x1fff;
+}
+
+static inline u32 ODEMUGetPage(u32 mail) { return (mail & 0xFF0000) >> 16; }
+
+static inline u32 ODEMUGetPc2NngcOffset(u32 mail) {
+    if (!(ODEMUGetPage(mail) & 0x1)) {
+        return 0;
+    }
+
+    return 0x800;
+}
+
+static inline BOOL ODEMUIsValidMail(u32 mail) {
+    return (mail & 0x1F000000) == 0x1F000000;
+}
+
+static inline u32 ODEMUGetSize(u32 mail) { return mail & 0x1FFF; }
+
+#ifdef __cplusplus
+}
+#endif
+#endif
