54b915a144455efea3a5cf6cf25a72025d2796a4
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2024-09-03 14:30:30 -0400

    Refactored `snd_WaveSound`

--- include/nw4r/snd/snd_WaveSound.h
+++ include/nw4r/snd/snd_WaveSound.h
@@ -1,42 +1,57 @@
-#ifndef NW4R_SND_WAVESOUND_H
-#define NW4R_SND_WAVESOUND_H
-#include "types_nw4r.h"
-#include "snd_BasicSound.h"
-#include "snd_WsdPlayer.h"
-#include "ut_RuntimeTypeInfo.h"
+#ifndef NW4R_SND_WAVE_SOUND_H
+#define NW4R_SND_WAVE_SOUND_H
+#include <nw4r/snd/snd_BasicSound.h>
+#include <nw4r/snd/snd_WsdPlayer.h>
+#include <nw4r/types_nw4r.h>
+#include <nw4r/ut.h>
 
-namespace nw4r
-{
-    namespace snd
-    {
-        namespace detail
-        {
-            struct WaveSound : BasicSound
-            {
-                WsdPlayer mPlayer; // at 0xD8
-                WaveSoundHandle *mTempSpecialHandle; // at 0x1B0
-                SoundInstanceManager<WaveSound> *mManager; // at 0x1B4
-                bool mIsPrepared; // at 0x1B8
+namespace nw4r {
+namespace snd {
+namespace detail {
 
-                WaveSound(SoundInstanceManager<WaveSound> *);
-                bool Prepare(const void *, long, WsdPlayer::StartOffsetType, s32, int, const WsdPlayer::WsdCallback *, u32);
-                void SetChannelPriority(int);
-                void SetReleasePriorityFix(bool);
+class WaveSound : public BasicSound {
+    friend class WaveSoundHandle;
 
-                static ut::detail::RuntimeTypeInfo typeInfo;
-                
-                virtual const ut::detail::RuntimeTypeInfo * GetRuntimeTypeInfo() const; // at 0x8
-                virtual ~WaveSound(); // at 0xc
-                virtual UNKTYPE Shutdown(); // at 0x28
-                virtual bool IsPrepared() const; // at 0x2c
-                virtual void SetPlayerPriority(int); // at 0x4c
-                virtual bool IsAttachedTempSpecialHandle(); // at 0x5c
-                virtual UNKTYPE DetachTempSpecialHandle(); // at 0x60
-                virtual BasicPlayer& GetBasicPlayer(); // at 0x68
-                virtual const BasicPlayer& GetBasicPlayer() const; // at 0x6c
-            };
-        }
-    }
-}
+public:
+    NW4R_UT_RTTI_DECL(WaveSound);
+
+public:
+    WaveSound(SoundInstanceManager<WaveSound>* pManager);
+    virtual ~WaveSound() {} // at 0xC
+
+    virtual void Shutdown(); // at 0x28
+    virtual bool IsPrepared() const {
+        return mPreparedFlag;
+    } // at 0x2C
+
+    virtual void SetPlayerPriority(int priority); // at 0x4C
+    virtual bool IsAttachedTempSpecialHandle();   // at 0x5C
+    virtual void DetachTempSpecialHandle();       // at 0x60
+
+    virtual BasicPlayer& GetBasicPlayer() {
+        return mWsdPlayer;
+    } // at 0x68
+    virtual const BasicPlayer& GetBasicPlayer() const {
+        return mWsdPlayer;
+    } // at 0x6C
+
+    bool Prepare(const void* pBase, s32 wsdOffset,
+                 WsdPlayer::StartOffsetType startType, s32 startOffset,
+                 int voices, const WsdPlayer::WsdCallback* pCallback,
+                 u32 callbackArg);
+
+    void SetChannelPriority(int priority);
+    void SetReleasePriorityFix(bool flag);
+
+private:
+    WsdPlayer mWsdPlayer;                      // at 0xD8
+    WaveSoundHandle* mTempSpecialHandle;       // at 0x1B0
+    SoundInstanceManager<WaveSound>* mManager; // at 0x1B4
+    bool mPreparedFlag;                        // at 0x1B8
+};
+
+} // namespace detail
+} // namespace snd
+} // namespace nw4r
 
 #endif
