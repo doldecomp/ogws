d7e3f667971519459577e938122f8c76330539c1
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2024-09-14 18:56:14 -0400

    Refactored `ResAnmShp` structure

--- include/nw4r/g3d/g3d_resanmshp.h
+++ include/nw4r/g3d/g3d_resanmshp.h
@@ -1,35 +1,48 @@
 #ifndef NW4R_G3D_RESANMSHP_H
 #define NW4R_G3D_RESANMSHP_H
-#include "types_nw4r.h"
-#include "g3d_rescommon.h"
+#include <nw4r/types_nw4r.h>
 
-namespace nw4r
-{
-	namespace g3d
-	{
-		struct ResAnmShpData
-		{
-			char UNK_0x0[0x8];
-			u32 mRevision; // at 0x8
-		};
+#include <nw4r/g3d/g3d_resanm.h>
+#include <nw4r/g3d/g3d_rescommon.h>
 
-		struct ResAnmShp
-		{
-			enum
-			{
-				REVISION = 3
-			};
-			
-			ResCommon<ResAnmShpData> mAnmShp;
-			
-			inline ResAnmShp(void * vptr) : mAnmShp(vptr) {}
-			
-			inline bool CheckRevision() const
-			{
-				return mAnmShp.ref().mRevision == REVISION;
-			}
-		};
-	}
-}
+namespace nw4r {
+namespace g3d {
+
+struct ResAnmShpInfoData {
+    u16 numFrame;     // at 0x0
+    u16 numVtxName;   // at 0x2
+    AnmPolicy policy; // at 0x4
+};
+
+struct ResAnmShpData {
+    ResBlockHeaderData header; // at 0x0
+    u32 revision;              // at 0x8
+    s32 toResFileData;         // at 0xC
+    s32 toShpDataDic;          // at 0x10
+    s32 toVtxNameArray;        // at 0x14
+    s32 name;                  // at 0x18
+    s32 original_path;         // at 0x1C
+    ResAnmShpInfoData info;    // at 0x20
+};
+
+class ResAnmShp : public ResCommon<ResAnmShpData> {
+public:
+    static const u32 SIGNATURE = 'SHP0';
+    static const int REVISION = 3;
+
+public:
+    ResAnmShp(void* pData) : ResCommon(pData) {}
+
+    u32 GetRevision() const {
+        return ref().revision;
+    }
+
+    bool CheckRevision() const {
+        return ref().revision == REVISION;
+    }
+};
+
+} // namespace g3d
+} // namespace nw4r
 
 #endif
