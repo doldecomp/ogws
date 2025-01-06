bdb8e4fec9d5ca3116a859ba5c574429a926f5ac
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-01-05 20:54:28 -0500

    *Actually* link the file :/

--- configure.py
+++ configure.py
@@ -521,7 +521,7 @@ config.libs = [
             Object(Matching, "nw4r/g3d/res/g3d_resanmlight.cpp"),
             Object(Matching, "nw4r/g3d/res/g3d_resanmfog.cpp"),
             Object(Matching, "nw4r/g3d/res/g3d_resanmcamera.cpp"),
-            Object(NonMatching, "nw4r/g3d/res/g3d_resanmscn.cpp"),
+            Object(Matching, "nw4r/g3d/res/g3d_resanmscn.cpp"),
             Object(Matching, "nw4r/g3d/res/g3d_resanmshp.cpp"),
             Object(Matching, "nw4r/g3d/g3d_anmvis.cpp"),
             Object(Matching, "nw4r/g3d/g3d_anmclr.cpp"),
