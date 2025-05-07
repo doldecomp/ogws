8bb1497de758ef7830a299dbcfd1ae868b000600
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-05-06 20:02:42 -0400

    Refactor eggDisposer

--- include/egg/core/eggDisposer.h
+++ include/egg/core/eggDisposer.h
@@ -1,16 +1,23 @@
 #ifndef EGG_CORE_DISPOSER_H
 #define EGG_CORE_DISPOSER_H
-#include "types_egg.h"
-#include "ut_list.h"
+#include <egg/types_egg.h>
+
+#include <nw4r/ut.h>
 
 namespace EGG {
-struct Disposer {
-    Disposer();
-    virtual ~Disposer();
 
-    Heap* mHeap;          // at 0x0
-    nw4r::ut::Link mLink; // at 0x4
+class Disposer {
+    friend class Heap;
+
+public:
+    Disposer();
+    virtual ~Disposer(); // at 0x8
+
+private:
+    Heap* mContainHeap;   // at 0x4
+    nw4r::ut::Link mLink; // at 0x8
 };
+
 } // namespace EGG
 
 #endif
