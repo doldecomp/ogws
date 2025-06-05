7364ce7fba084eb4139d5a1d69876dea28db5335
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-06-05 17:33:04 -0400

    Math types work

--- include/egg/math/eggMatrix.h
+++ include/egg/math/eggMatrix.h
@@ -1,42 +1,72 @@
 #ifndef EGG_MATH_MATRIX_H
 #define EGG_MATH_MATRIX_H
-#include "types_egg.h"
+#include <egg/types_egg.h>
 
-namespace EGG
-{
-    struct Matrix33f
-    {
-        f32& operator()(int i, int j) { return tbl[i][j]; }
+#include <nw4r/math.h>
 
-        f32 tbl[3][3];
-    };
+namespace EGG {
 
-    struct Matrix34f
-    {
-        Matrix34f() {}
-        Matrix34f(f32, f32, f32, f32,
-            f32, f32, f32, f32,
-            f32, f32, f32, f32);
+class Matrix34f : public nw4r::math::MTX34 {
+public:
+    static Matrix34f ident;
 
-        void makeZero()
-        {
-            tbl[0][3] = tbl[0][2] = tbl[0][1] = tbl[0][0] = 0.0f;
-            tbl[1][3] = tbl[1][2] = tbl[1][1] = tbl[1][0] = 0.0f;
-            tbl[2][3] = tbl[2][2] = tbl[2][1] = tbl[2][0] = 0.0f;
+public:
+    Matrix34f() {}
+
+    // clang-format off
+    Matrix34f(f32 m00, f32 m01, f32 m02, f32 m03,
+              f32 m10, f32 m11, f32 m12, f32 m13,
+              f32 m20, f32 m21, f32 m22, f32 m23);
+    // clang-format on
+
+    void makeZero() {
+        _03 = _02 = _01 = _00 = 0.0f;
+        _13 = _12 = _11 = _10 = 0.0f;
+        _23 = _22 = _21 = _20 = 0.0f;
+    }
+
+    f32& operator()(int i) {
+        return a[i];
+    }
+    const f32& operator()(int i) const {
+        return a[i];
+    }
+
+    f32& operator()(int i, int j) {
+        return m[i][j];
+    }
+    const f32& operator()(int i, int j) const {
+        return m[i][j];
+    }
+
+    void copyFrom(const Matrix34f& rOther) {
+        for (int i = 0; i < ARRAY_SIZE(a); i++) {
+            (*this)(i) = rOther(i);
         }
+    }
 
-        void makeIdentity();
-        void makeQ(const Quatf&);
-        void makeS(const Vector3f&);
-        void setAxisRotation(const Vector3f &, f32);
-        void loadPosMtx(u32);
+    /*
+    getBase__Q23EGG9Matrix34fCFiRQ23EGG8Vector3f
+    setBase__Q23EGG9Matrix34fFiRCQ23EGG8Vector3f
 
-        f32& operator()(int i, int j) { return tbl[i][j]; }
+    getTranslation__Q23EGG9Matrix34fCFRQ23EGG8Vector3f
+    setTranslation__Q23EGG9Matrix34fFRCQ23EGG8Vector3f
 
-        f32 tbl[3][4];
+    multVectorTo__Q23EGG9Matrix34fCFRCQ23EGG8Vector3fRQ23EGG8Vector3f
 
-        static Matrix34f ident;
-    };
-}
+    concat__Q23EGG9Matrix34fCFRCQ23EGG9Matrix34fRQ23EGG9Matrix34f
+    rotate__Q23EGG9Matrix34fCFRCQ23EGG8Vector3f
+    */
+
+    void makeIdentity();
+
+    void makeQ(const Quatf& rQuat);
+    void makeS(const Vector3f& rScale);
+
+    void setAxisRotation(const Vector3f& rAxis, f32 angle);
+    void loadPosMtx(u32 id);
+};
+
+} // namespace EGG
 
 #endif
--- src/egg/math/eggMatrix.cpp
+++ src/egg/math/eggMatrix.cpp
@@ -1,116 +1,100 @@
-#include "eggMatrix.h"
-#include "eggVector.h"
-#include "eggQuat.h"
+// TODO: REMOVE AFTER REFACTOR
+#pragma ipa file
+
+#include <egg/math.h>
+
 #include <revolution/GX.h>
 
-namespace EGG
-{
+namespace EGG {
 
-    // This weak destructor likely belongs to Quatf due to the BBA map
-    // and that this is referenced in the stack unwinding instructions of setAxisRotation.
-    namespace
-    {
-        void INSTANTIATE_WEAK_EGGMATRIX()
-        {
-            Quatf q;
-            q.~Quatf();
-        }
-    }
+// clang-format off
+Matrix34f Matrix34f::ident(1.0f, 0.0f, 0.0f, 0.0f,
+                           0.0f, 1.0f, 0.0f, 0.0f,
+                           0.0f, 0.0f, 1.0f, 0.0f);
+// clang-format on
 
-    Matrix34f::Matrix34f(f32 _00, f32 _01, f32 _02, f32 _03,
-        f32 _10, f32 _11, f32 _12, f32 _13,
-        f32 _20, f32 _21, f32 _22, f32 _23)
-    {
-        tbl[0][0] = _00; tbl[0][1] = _01; tbl[0][2] = _02; tbl[0][3] = _03;
-        tbl[1][0] = _10; tbl[1][1] = _11; tbl[1][2] = _12; tbl[1][3] = _13;
-        tbl[2][0] = _20; tbl[2][1] = _21; tbl[2][2] = _22; tbl[2][3] = _23;
-    }
+DECOMP_FORCEACTIVE_DTOR(eggMatrix_cpp,
+                        Quatf);
 
-    void Matrix34f::makeIdentity()
-    {
-        // Force float order in sdata2
-        1.0f;
-        2.0f;
-        0.0f;
+DECOMP_FORCELITERAL(eggMatrix_cpp_1,
+                    1.0f, 2.0f, 0.0f);
 
-        makeZero();
-        tbl[2][2] = 1.0f;
-        tbl[1][1] = 1.0f;
-        tbl[0][0] = 1.0f;
-    }
+Matrix34f::Matrix34f(f32 m00, f32 m01, f32 m02, f32 m03, f32 m10, f32 m11,
+                     f32 m12, f32 m13, f32 m20, f32 m21, f32 m22, f32 m23) {
 
-   /**
-    * Converts a quaternion to a rotation matrix (3x4)
-    * [1 - 2Y^2 - 2Z^2] [   2XY - 2ZW   ] [   2XZ + 2YW   ] [   UNUSED   ]
-    * [   2XY + 2ZW   ] [1 - 2X^2 - 2Z^2] [   2YZ - 2XW   ] [   UNUSED   ]
-    * [   2XZ - 2YW   ] [   2YZ + 2XW   ] [1 - 2X^2 - 2Y^2] [   UNUSED   ]
-    * 
-    * Implementation from SEAD decompilation: https://github.com/open-ead/sead/blob/master/include/math/seadMatrixCalcCommon.hpp#L1179
-    */
-    void Matrix34f::makeQ(const Quatf& quat)
-    {
-        f32 yy = 2.0f * quat.y * quat.y;
-        f32 zz = 2.0f * quat.z * quat.z;
-        f32 xx = 2.0f * quat.x * quat.x;
-        f32 xy = 2.0f * quat.x * quat.y;
-        f32 xz = 2.0f * quat.x * quat.z;
-        f32 yz = 2.0f * quat.y * quat.z;
-        f32 wz = 2.0f * quat.w * quat.z;
-        f32 wx = 2.0f * quat.w * quat.x;
-        f32 wy = 2.0f * quat.w * quat.y;
-
-        tbl[0][0] = 1.0f - yy - zz;
-        tbl[0][1] = xy - wz;
-        tbl[0][2] = xz + wy;
-
-        tbl[1][0] = xy + wz;
-        tbl[1][1] = 1.0f - xx - zz;
-        tbl[1][2] = yz - wx;
-
-        tbl[2][0] = xz - wy;
-        tbl[2][1] = yz + wx;
-        tbl[2][2] = 1 - xx - yy;
-
-        tbl[0][3] = 0.0f;
-        tbl[1][3] = 0.0f;
-        tbl[2][3] = 0.0f;
-    }
-
-    void Matrix34f::makeS(const Vector3f& vec)
-    {
-        tbl[0][0] = vec.x;
-        tbl[0][1] = 0.0f;
-        tbl[0][2] = 0.0f;
-
-        tbl[1][0] = 0.0f;
-        tbl[1][1] = vec.y;
-        tbl[1][2] = 0.0f;
-
-        tbl[2][0] = 0.0f;
-        tbl[2][1] = 0.0f;
-        tbl[2][2] = vec.z;
-
-        tbl[0][3] = 0.0f;
-        tbl[1][3] = 0.0f;
-        tbl[2][3] = 0.0f;
-    }
-
-    void Matrix34f::setAxisRotation(const Vector3f & vec, f32 f1)
-    {
-        Quatf q;
-        
-        q.setAxisRotation(vec, f1);
-        makeQ(q);
-    }
-
-    void Matrix34f::loadPosMtx(u32 i)
-    {
-        GXLoadPosMtxImm(tbl, i);
-    }
-
-    Matrix34f Matrix34f::ident(
-        1.0f, 0.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f, 0.0f,
-        0.0f, 0.0f, 1.0f, 0.0f
-    );
+    _00 = m00;
+    _01 = m01;
+    _02 = m02;
+    _03 = m03;
+    _10 = m10;
+    _11 = m11;
+    _12 = m12;
+    _13 = m13;
+    _20 = m20;
+    _21 = m21;
+    _22 = m22;
+    _23 = m23;
 }
+
+void Matrix34f::makeIdentity() {
+    makeZero();
+    _00 = _11 = _22 = 1.0f;
+}
+
+void Matrix34f::makeQ(const Quatf& rQuat) {
+    f32 yy = 2 * rQuat.v.y * rQuat.v.y;
+    f32 zz = 2 * rQuat.v.z * rQuat.v.z;
+    f32 xx = 2 * rQuat.v.x * rQuat.v.x;
+    f32 xy = 2 * rQuat.v.x * rQuat.v.y;
+    f32 xz = 2 * rQuat.v.x * rQuat.v.z;
+    f32 yz = 2 * rQuat.v.y * rQuat.v.z;
+    f32 wz = 2 * rQuat.w * rQuat.v.z;
+    f32 wx = 2 * rQuat.w * rQuat.v.x;
+    f32 wy = 2 * rQuat.w * rQuat.v.y;
+
+    _00 = 1 - yy - zz;
+    _01 = xy - wz;
+    _02 = xz + wy;
+
+    _10 = xy + wz;
+    _11 = 1 - xx - zz;
+    _12 = yz - wx;
+
+    _20 = xz - wy;
+    _21 = yz + wx;
+    _22 = 1 - xx - yy;
+
+    _03 = 0.0f;
+    _13 = 0.0f;
+    _23 = 0.0f;
+}
+
+void Matrix34f::makeS(const Vector3f& rScale) {
+    _00 = rScale.x;
+    _01 = 0.0f;
+    _02 = 0.0f;
+
+    _10 = 0.0f;
+    _11 = rScale.y;
+    _12 = 0.0f;
+
+    _20 = 0.0f;
+    _21 = 0.0f;
+    _22 = rScale.z;
+
+    _03 = 0.0f;
+    _13 = 0.0f;
+    _23 = 0.0f;
+}
+
+void Matrix34f::setAxisRotation(const Vector3f& rAxis, f32 angle) {
+    Quatf q;
+    q.setAxisRotation(rAxis, angle);
+    makeQ(q);
+}
+
+void Matrix34f::loadPosMtx(u32 id) {
+    GXLoadPosMtxImm(m, id);
+}
+
+} // namespace EGG
