131fe13af461755526d5ab58afcf36c673798acf
Alex9303 <Alex9303Github@protonmail.com>
2025-09-23 21:28:59 -0400

    ansi_fp

--- include/MSL/ansi_fp.h
+++ include/MSL/ansi_fp.h
@@ -1,6 +1,25 @@
 #ifndef ANSI_FP_H
 #define ANSI_FP_H
 #include <types.h>
+
+typedef struct decimal {
+    char sign;
+    char _1;
+    short exponent;
+
+    struct {
+        unsigned char length;
+        unsigned char text[36];
+        unsigned char pad;
+    } sig;
+} decimal;
+
+typedef struct decform {
+    char style;
+    char _1;
+    short digits;
+} decform;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
