a0f29fc9ed1fcba5cafa8b85f280963be8fa1207
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-11-22 15:16:52 -0500

    Split RPSysKokeshiIterater

--- configure.py
+++ configure.py
@@ -1069,6 +1069,7 @@ config.libs = [
             Object(NonMatching, "Pack/RPKernel/RPSysMiddleDB.cpp"),
             Object(NonMatching, "Pack/RPKernel/RPSysLytAllocator.cpp"),
             Object(NonMatching, "Pack/RPKernel/RPSysLytDynamicAnm.cpp"),
+            Object(NonMatching, "Pack/RPKernel/RPSysKokeshiIterater.cpp"),
         ],
     },
 ]
