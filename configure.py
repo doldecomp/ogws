bfc8469fa030004b88b1728b729f86923e3be637
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-11-22 13:36:35 -0500

    Split RPSysEffectMgr

--- configure.py
+++ configure.py
@@ -1045,6 +1045,7 @@ config.libs = [
             Object(NonMatching, "Pack/RPKernel/RPSysKokeshi.cpp"),
             Object(NonMatching, "Pack/RPKernel/RPSysKokeshiManager.cpp"),
             Object(NonMatching, "Pack/RPKernel/RP_DEBUG_STUB_1.cpp"),
+            Object(NonMatching, "Pack/RPKernel/RPSysEffectMgr.cpp"),
         ],
     },
 ]
