9d0746bc4daf2fe3fc3cfc66cda8ccf26077b9ca
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2025-11-22 15:01:27 -0500

    Split RPSysKokeshiGenInfo

--- configure.py
+++ configure.py
@@ -1062,6 +1062,7 @@ config.libs = [
             Object(NonMatching, "Pack/RPKernel/RPSysTagProcessor.cpp"),
             Object(NonMatching, "Pack/RPKernel/RPSysController.cpp"),
             Object(NonMatching, "Pack/RPKernel/RPSysKokeshiIcon.cpp"),
+            Object(NonMatching, "Pack/RPKernel/RPSysKokeshiGenInfo.cpp"),
         ],
     },
 ]
