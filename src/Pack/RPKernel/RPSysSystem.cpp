b8b1655f6e26aa974b27ae8207bd45202eaa3a96
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2026-08-29 15:33:29 -0400

    Update RPSysSystem.cpp

--- src/Pack/RPKernel/RPSysSystem.cpp
+++ src/Pack/RPKernel/RPSysSystem.cpp
@@ -1,10 +1,12 @@
-#include <Pack/RPSystem.h>
 #include <Pack/RPKernel.h>
+#include <Pack/RPSystem.h>
+
 #include <egg/core.h>
 #include <egg/gfxe.h>
+
+#include <revolution/OS.h>
 #include <revolution/SC.h>
 #include <revolution/VI.h>
-#include <revolution/OS.h>
 
 void RPSysSystem::setupTVMode() {
     int i;
@@ -18,15 +20,14 @@ void RPSysSystem::setupTVMode() {
     }
     if (SCGetAspectRatio() == SC_ASPECT_WIDE) {
         EGG::Screen::SetTVMode(EGG::Screen::TV_MODE_WIDE);
-    }
-    else {
+    } else {
         EGG::Screen::SetTVMode(EGG::Screen::TV_MODE_STD);
     }
 }
 
 RPSysSceneCreator::ESceneID RPSysSystem::getBootScene() {
     // this function is hardcoded (symbol name is official, weirdly enough)
-    return RPSysSceneCreator::ESceneID_RPSportsTitleScene;
+    return RPSysSceneCreator::ESceneID_RPSportsPackTitleScene;
 }
 
 void RPSysSystem::startLoadCount() {
@@ -41,21 +42,27 @@ void RPSysSystem::setupLocalSettings() {
     u8 sysLanguage = SCGetLanguage();
 
     if (sysLanguage == SC_LANG_EN) {
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLocale(RPSysProjectLocal::EArea_USA);
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLanguage(RPSysProjectLocal::EArea_USA);
-    }
-    else if (sysLanguage == SC_LANG_FR) {
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLocale(RPSysProjectLocal::EArea_USA);
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLanguage(RPSysProjectLocal::EArea_France);
-    }
-    else if (sysLanguage == SC_LANG_SP) {
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLocale(RPSysProjectLocal::EArea_USA);
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLanguage(RPSysProjectLocal::EArea_Spain);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLocale(RPSysProjectLocal::EArea_USA);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLanguage(RPSysProjectLocal::EArea_USA);
+    } else if (sysLanguage == SC_LANG_FR) {
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLocale(RPSysProjectLocal::EArea_USA);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLanguage(RPSysProjectLocal::EArea_France);
+    } else if (sysLanguage == SC_LANG_SP) {
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLocale(RPSysProjectLocal::EArea_USA);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLanguage(RPSysProjectLocal::EArea_Spain);
     }
     //! Requires ifdef for non Americas versions
     else {
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLocale(RPSysProjectLocal::EArea_USA);
-        RP_GET_INSTANCE(RPSysProjectLocal)->setLanguage(RPSysProjectLocal::EArea_USA);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLocale(RPSysProjectLocal::EArea_USA);
+        RP_GET_INSTANCE(RPSysProjectLocal)
+            ->setLanguage(RPSysProjectLocal::EArea_USA);
     }
 }
 
@@ -63,7 +70,6 @@ void RPSysSystem::setupLocalSettings() {
  * @brief Controls whether the game restarts or returns to the Wii Menu upon a
  * soft reset.
  */
-
 void RPSysSystem::softResetCallBack() {
     if (RP_GET_INSTANCE(RPSysDvdStatus)->isErrorOccured()) {
         RP_GET_INSTANCE(RPSysSceneMgr)->returnToMenu(FALSE);
