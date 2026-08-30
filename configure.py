37fc77eb2a941435a41b654fa0cba2f4993b9f1e
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2026-08-30 17:53:53 -0400

    Split RPUtlDirMath

--- configure.py
+++ configure.py
@@ -1307,6 +1307,7 @@ config.libs = [
             Object(NonMatching, "Pack/RPUtility/RPUtlCollisionChecker.cpp"),
             Object(NonMatching, "Pack/RPUtility/RPUtlSimpleShadow.cpp"),
             Object(NonMatching, "Pack/RPUtility/RPUtlGameResourceMgr.cpp"),
+            Object(NonMatching, "Pack/RPUtility/RPUtlDirMath.cpp"),
             Object(NonMatching, "Pack/RPUtility_unsplit.o"),
         ],
     },
